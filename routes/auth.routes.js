b59ded299e63a22884bf1ce49ff1c479b564e745
isaac12458 <isaac@clintondev.localdomain>
2026-02-18 16:58:34 +0100

    Auth system with JWT, refresh tokens, and protected routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,6 +2,7 @@ const generateToken = require("../utils/generateToken");
 const jwt = require("jsonwebtoken");
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 const User = require("../models/User");
 
 /**
